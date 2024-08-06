8ad002659f5ddd0fbe64752139d9c2f96eba1c16
Julia Hromada <hromada.julia@gmail.com>
2024-08-06 09:19:31 +0200

    social media challenge done

--- java-script/variables-numbers/js-variables-and-numbers_social-media-post/js/index.js
+++ java-script/variables-numbers/js-variables-and-numbers_social-media-post/js/index.js
@@ -1,7 +1,8 @@
 console.clear();
 
 /*
-1. Create the data for a small social media post. Create a const variable for each data point listed below.:
+1. Create the data for a small social media post. 
+Create a const variable for each data point listed below.:
 	- a title
 	- a text
 	- the number of likes
@@ -10,12 +11,25 @@ console.clear();
 */
 
 // --v-- write your code here --v--
+const title = "Title";
+const text = "I like...";
+let NumberOfLikes = 0;
+const user = "me";
+const isReported = false;
 
 // --^-- write your code here --^--
 
 /*
-2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
+2. Log all variables to the console. 
+Increase the likes by one and log the amount of likes again.
+Adapt your code from 1.1 if necessary.
 */
+console.log("Title: ", title);
+console.log("Text: ", text);
+console.log("Likes ", NumberOfLikes);
+
+NumberOfLikes++;
+console.log("Likes ", NumberOfLikes);
 
 // --v-- write your code here --v--
 
