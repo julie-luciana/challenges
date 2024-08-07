97eeddf47737e26d9d8bcbec3b54c9dae44d68cd
Julia Hromada <hromada.julia@gmail.com>
2024-08-07 08:42:03 +0200

    math funcions

--- js-functions/js-functions_functions-with-parameters-extra/js/index.js
+++ js-functions/js-functions_functions-with-parameters-extra/js/index.js
@@ -1,36 +1,58 @@
 console.clear();
 
 /*
-1: The function calls below do not work properly yet. Make the function
-   printSquare log the square of the parameter to the console.
+1: The function calls below do not work 
+properly yet. Make the function
+   printSquare log the square of the 
+   parameter to the console.
    The formula is: square = number * number
 */
 
-function printSquare(number) {}
+function printSquare(number) {
+  square = number * number;
+  console.log(square);
+}
 
 printSquare(3);
 printSquare(5);
 
 /*
-2: We want to use a function which accepts the radius of a circle as a parameter and logs
-   the circumference to the console. The function does not exist yet.
-   The formula is: circumference = 2 * Pi * radius
+2: We want to use a function which accepts 
+the radius of a circle as a parameter and logs
+   the circumference to the console. 
+   The function does not exist yet.
+   The formula is: 
+   circumference = 2 * Pi * radius
 */
+function printCircumference(radius) {
+  /* circumference = 2 * Math.PI * radius;
+  console.log(circumference);
+  */
+  console.log(2 * Math.PI * radius);
+}
+// Uncomment the following
+//function calls and implement the
+//function printCircumference
 
-// Uncomment the following function calls and implement the function printCircumference
-
-// printCircumference(4);
-// printCircumference(6);
+printCircumference(4);
+printCircumference(6);
 
 /*
-3: We want to use a function which accepts the width and length of a rectangle
-   and prints the following text to the console: "The area of the rectangle is ?".
-   The function should print the correct area instead of the question mark "?".
+3: We want to use a function which 
+accepts the width and length of a rectangle
+   and prints the following text to the 
+   console: "The area of the rectangle is ?".
+   The function should print the correct 
+   area instead of the question mark "?".
    The function does not exist yet.
    The formula is: area = width * length
 */
 
+function printRectangleArea(width, length) {
+  area = width * length;
+  console.log("The area of the rectangle is " + area);
+}
 // Uncomment the following function calls and implement the function printRectangleArea
 
-// printRectangleArea(5, 7);
-// printRectangleArea(3, 4);
+printRectangleArea(5, 7);
+printRectangleArea(3, 4);
