d030d3121aaf33e0fd871e8e97770088d17d0c6c
Julia Hromada <hromada.julia@gmail.com>
2024-08-12 20:22:24 +0200

    pizza analyzer

--- js-functions/js-functions_pizza-analyzer/js/index.js
+++ js-functions/js-functions_pizza-analyzer/js/index.js
@@ -1,4 +1,4 @@
-console.clear();
+// console.clear();
 
 const pizzaInput1 = document.querySelector('[data-js="pizza-input-1"]');
 const pizza1 = document.querySelector('[data-js="pizza-1"]');
@@ -7,20 +7,47 @@ const pizza2 = document.querySelector('[data-js="pizza-2"]');
 const outputSection = document.querySelector('[data-js="output-section"]');
 
 const output = document.querySelector('[data-js="output"]');
+console.log(outputSection);
 
 pizzaInput1.addEventListener("input", () => {
-  // write your code here
+  console.log("input 1");
+  const pizzaSize1 = pizzaInput1.value;
+  const pizzaSize2 = pizzaInput2.value;
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
+  updatePizzaDisplay(pizza1, pizzaSize1);
+  updateOutputColor(pizzaSize1, pizzaSize2);
 });
 
 pizzaInput2.addEventListener("input", () => {
-  // write your code here
+  console.log("input 2");
+  const pizzaSize1 = pizzaInput1.value;
+  const pizzaSize2 = pizzaInput2.value;
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
+  updatePizzaDisplay(pizza2, pizzaSize2);
+  updateOutputColor(pizzaSize1, pizzaSize2);
 });
 
 // Task 1
-// define the function calculatePizzaGain here
+
+function calculatePizzaGain(diameter1, diameter2) {
+  const pizzaArea1 = (diameter1 / 2) ** 2 * Math.PI;
+  const pizzaArea2 = (diameter2 / 2) ** 2 * Math.PI;
+  output.textContent = Math.round(
+    ((pizzaArea2 - pizzaArea1) / pizzaArea1) * 100
+  );
+}
 
 // Task 2
-// define the function updatePizzaDisplay here
+
+function updatePizzaDisplay(pizzaElement, newSize) {
+  const result = (newSize / 24) * 100;
+  pizzaElement.style.width = result + "px";
+}
 
 // Task 3
-// define the function updateOutputColor here
+
+function updateOutputColor(size1, size2) {
+  outputSection.style.backgroundColor = `var(${
+    size1 <= size2 ? "--green" : "--red"
+  }`;
+}
