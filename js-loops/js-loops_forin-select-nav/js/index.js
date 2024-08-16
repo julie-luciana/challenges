744a2e1ee9bd43a7c96e2c6a759865b66ac2ade5
Julia Hromada <hromada.julia@gmail.com>
2024-08-16 19:34:34 +0200

    challenge done

--- js-loops/js-loops_forin-select-nav/js/index.js
+++ js-loops/js-loops_forin-select-nav/js/index.js
@@ -21,9 +21,27 @@ select.name = "languages";
 main.append(select);
 
 // --v-- write/change code here --v--
+/*
+Iterate through the `languages` object using `for...in` and 
+add the `option` elements for this dropdown menu.
+
+The following hints may guide you:
+
+- Create one `option` element for each entry in `languages`.
+- For each option, set its `value` and `textContent` to the value 
+of each entry in `languages`. 
+You will need bracket notation, e.g. `languages[myIteratorName]`.
+- Don't forget to append each option!
+ */
+
+for (const key in languages) {
+  let option = document.createElement("option");
+  option.value = languages[key];
+  option.textContent = languages[key];
+  select.append(option);
+}
 
 // --^-- write/change code here --^--
-
 // Part 2: Creating a Navigation Bar
 
 const nav = {
@@ -38,5 +56,31 @@ main.append(navElement);
 navElement.append(ul);
 
 // --v-- write/change code here --v--
+/*
+Before we create a navigation bar, let's have a look what's already there in `./js/index.js` :
 
+- There is a `nav` object with the keys "home", "about", and "contact".
+- Each value is an object containing an "href" and "text" key and a string value.
+- A `nav` and `ul` element is created and appended to the `main` element.
+
+Your task is to fill this list with list items and anchor tags; as content, use the `nav` object which you can access with a `for...in` loop.
+
+Use the following hints as a guideline:
+
+- Write a `for...in` loop for the `nav` object.
+- In each iteration,
+  - create a list item and an anchor tag
+  - set the anchor tag's `href` and `textContent` properties to the relevant values of the `nav` object; you want to use bracket notation with your iterator variable
+  - append your created items accordingly.
+ */
+for (key in nav) {
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+
+  a.setAttribute("href", nav[key].href);
+  a.textContent = nav[key].text;
+
+  li.append(a);
+  ul.append(li);
+}
 // --^-- write/change code here --^--
