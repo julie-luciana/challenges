1e2a8e6d04bfc735bd3bf1d6437fec3435c57654
Julia Hromada <hromada.julia@gmail.com>
2024-08-25 22:15:36 +0200

    .

--- callback-functions/js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
+++ callback-functions/js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
@@ -1,11 +1,51 @@
-console.clear();
+/*
+Create a function called calculateTotalPrice that takes two parameters products 
+(an array of products) and extractPrice (a callback function).
 
-function handleUserLogin(onSuccess, onError, userName) {
-  if (userName === "Jane Doe") {
-    onSuccess(userName, "admin");
-  } else {
-    onError(`Unknown user "${userName}"`);
+Inside the calculateTotalPrice function, initialize a variable totalPrice to 
+keep track of the total price and set its initial value to 0.
+
+Use a loop, such as a for...of loop, to iterate through the array of products.
+
+In each iteration of the loop, call the provided callback function with the current 
+product as an argument to extract its price.
+
+Add the extracted price to the totalPrice variable in each iteration of the loop.
+
+After looping through all the products, return the total price as the 
+result of the calculateTotalPrice function.
+
+Create the callback function extractPrice, which should take a product as 
+input and return its price.
+
+Use the calculateTotalPrice function with the array of products and the callback 
+function to calculate the total price.
+*/
+const products = [
+  { name: "Saffron", price: 50 },
+  { name: "Cinnamon", price: 6 },
+  { name: "Cardamom", price: 12 },
+  { name: "Turmeric", price: 9 },
+  { name: "Paprika", price: 4 },
+  { name: "Garam Masala", price: 10 },
+  { name: "Vanilla Bean", price: 15 },
+  { name: "Coriander", price: 6 },
+];
+
+function calculateTotalPrice(product, extractPrice) {
+  const totalPrice = 2;
+  const newPrice = totalPrice + product.price;
+  console.log(`The total price of ${product.name} is ` + newPrice);
+}
+function extractPrice(product) {
+  console.log(`The price of ${product.name} is ${product.price}.`);
+}
+for (const product of products) {
+  for (let i = 0; i < products.length; i++) {
+    const productNumber = i + 1;
+    console.log(`Produkt Nr.`, productNumber, `Name: ${products[i].name}`);
+    extractPrice(product);
+    calculateTotalPrice(product);
+    console.log("_______________");
   }
 }
-
-// The exercise starts here!
