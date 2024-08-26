ebf1e639e5bd85e55c511339c3b1a4b0d78b438d
Julia Hromada <hromada.julia@gmail.com>
2024-08-26 16:11:21 +0200

    .

--- react-basics/react-basics_hello-world/src/App.jsx
+++ react-basics/react-basics_hello-world/src/App.jsx
@@ -1,5 +1,19 @@
 import "./App.css";
+/**Welcome to the React Universe!
+Have a look at the [App.jsx](./src/App.jsx) file: you will find a default 
+export function called `App` which returns a `<div>`.
 
+Once running in your browser, the content of the `<div>` ("Say hello...") 
+should be visible there as well.
+Now, let us cherish a tradition and render "Hello world!". */
+
+// Task 1
+
+// Task 2: Create Your First Custom Component
 export default function App() {
-  return <div>Say hello...</div>;
+  return <HelloWorld />;
+}
+
+function HelloWorld() {
+  return <h1>Hello world!</h1>;
 }
