3ea703d34703beaaaaba9d11d51ddaff4339ff59
Julia Hromada <hromada.julia@gmail.com>
2024-08-27 16:18:54 +0200

    .

--- react-state/react-state_box/src/App.jsx
+++ react-state/react-state_box/src/App.jsx
@@ -21,8 +21,7 @@ export default function App() {
 }
 
 // Bonus Task
-/* 
-The console.log is in the handleClick-function, which turns the state of 
+/* the console.log is in the handleClick-function, which turns the state of 
 isActive and console.log's the old/opposite value.   
-It is updated in the next call, but then it is updated again, 
-thats why it's always vice vera.*/
+It is updated in the next call, but then it is updated again, thats why it's always
+vice vera.*/
