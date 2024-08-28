e91381c5e1897f4983238e92649be80c4b57e5cc
Julia Hromada <hromada.julia@gmail.com>
2024-08-28 15:22:38 +0200

    .

--- react-with-arrays/react-with-arrays_fruits/src/App.jsx
+++ react-with-arrays/react-with-arrays_fruits/src/App.jsx
@@ -2,39 +2,35 @@ import Card from "./components/Card";
 import "./App.css";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      id: 1,
+      name: "🍌 Banana",
+      color: "yellow",
+    },
+    {
+      id: 2,
+      name: "🍒 Cherry",
+      color: "red",
+    },
+    {
+      id: 3,
+      name: "🥭 mango",
+      color: "orange",
+    },
+    {
+      id: 4,
+      name: "🫐 blueberry",
+      color: "blue",
+    },
+    {
+      id: 5,
+      name: "🥑 avocado",
+      color: "green",
+    },
+  ];
 
-  const fruit1 = {
-    id: 1337,
-    name: "🍌 Banana",
-    color: "yellow",
-  };
-  const fruit2 = {
-    id: 2,
-    name: "🍒 Cherry",
-    color: "red",
-  };
-  const fruit3 = {
-    id: 3,
-    name: "🥭 mango",
-    color: "orange",
-  };
-  const fruit4 = {
-    id: 4,
-    name: "🫐 blueberry",
-    color: "blue",
-  };
-  const fruit5 = {
-    id: 5,
-    name: "🥑 avocado",
-    color: "green",
-  };
-  fruits.push(fruit1);
-  fruits.push(fruit2);
-  fruits.push(fruit3);
-  fruits.push(fruit4);
-  fruits.push(fruit5);
-  console.log(fruits);
+  console.log(fruits.map((fruit) => fruit.name));
 
   return (
     <div className="app">
