4a65c59acc938d5d8b1a79f0369f6ab263d3c557
Julia Hromada <hromada.julia@gmail.com>
2024-09-04 12:06:55 +0200

    display links to all volumes on Volumes Page

--- lotr-app/pages/volumes/index.js
+++ lotr-app/pages/volumes/index.js
@@ -1,7 +1,25 @@
+import Link from "next/link";
+
 export default function Volumes() {
   return (
     <>
       <h1>Volumes-Page </h1>
+      <p>Purpose: Display list with links to all volumes</p>
+      <ul>
+        <li>
+          <Link href="/volumes/the-fellowship-of-the-ring">
+            the fellowship of the ring
+          </Link>
+        </li>
+        <li>
+          <Link href="/volumes/the-two-towers">the two towers</Link>
+        </li>
+        <li>
+          <Link href="/volumes/the-return-of-the-king">
+            the return of the king
+          </Link>
+        </li>
+      </ul>
     </>
   );
 }
