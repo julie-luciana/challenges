6d041df2b8519dd65d712e445f72cd1e7dac2152
Julia Hromada <hromada.julia@gmail.com>
2024-09-04 12:26:36 +0200

    overview Page contents done

--- lotr-app/pages/volumes/index.js
+++ lotr-app/pages/volumes/index.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
-
+import { introduction } from "resources/lib/data";
 export default function Volumes() {
   return (
     <>
-      <h1>Volumes-Page </h1>
+      <h1>Lord of the Rings</h1>
+      <p>{introduction} </p>
+      <h2>All Volumes</h2>
       <p>Purpose: Display list with links to all volumes</p>
       <ul>
         <li>
