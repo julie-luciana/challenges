1947108c956ea63c5b1e1b5ce2e7879723abb078
Julia Hromada <hromada.julia@gmail.com>
2024-09-04 16:42:51 +0200

    starting dynamic routes challenge now

---
+++ dynamic-routes/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import { introduction, volumes } from "../../lib/data"; // volumes importieren
+
+export default function Volumes() {
+  return (
+    <>
+      <h1>The Lord of the Rings</h1>
+      <p>{introduction}</p>
+      <ul>
+        {volumes.map(({ slug, title }) => (
+          <li key={slug}>
+            <Link href={`/volumes/${slug}`}>{title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
