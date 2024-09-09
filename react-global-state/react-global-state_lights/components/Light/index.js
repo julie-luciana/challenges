205f14bb5e2b31f9a7629fcc68ed3ca67575d985
Julia Hromada <hromada.julia@gmail.com>
2024-09-09 09:24:38 +0200

    fist part of challenge, but still does not work as before

--- react-global-state/react-global-state_lights/components/Light/index.js
+++ react-global-state/react-global-state_lights/components/Light/index.js
@@ -1,21 +1,8 @@
-import { useState } from "react";
 import { LightButton, Icon, Text, Name, State } from "./Light.styled";
 
-export default function Light({ name }) {
-  const [isOn, setIsOn] = useState(false);
-
-  function handleToggle() {
-    setIsOn((isOn) => !isOn);
-  }
-
+export default function Light({ isOn, name, onToggleIsOn }) {
   return (
-    <LightButton
-      type="button"
-      onClick={() => {
-        handleToggle();
-      }}
-      $isOn={isOn}
-    >
+    <LightButton type="button" onClick={onToggleIsOn} $isOn={isOn}>
       <Icon $isOn={isOn}>💡</Icon>
       <Text>
         <Name>{name}</Name>
