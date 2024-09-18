5fd2a5b51cd7f748f02a1679c07a401cabaacaeb
Julia Hromada <hromada.julia@gmail.com>
2024-09-18 16:05:08 +0100

    downloaded challenges

--- backend-read/backend-read_products/pages/api/products/index.js
+++ backend-read/backend-read_products/pages/api/products/index.js
@@ -1,14 +1,5 @@
-//Import dbConnect from the db/connect.js file.
-//Import the Product model
+import { products } from "../../../lib/products";
 
-import dbConnect from "@/db/connect";
-import Product from "@/db/models/Product";
-// Make the handler function async and await the connection to the database.
-export default async function handler(request, response) {
-  await dbConnect();
-  // If the request.method is GET use the Product model to find all products and return them as a response
-  if (request.method === "GET") {
-    const products = await Product.find();
-    response.status(200).json(products);
-  }
+export default function handler(request, response) {
+  return response.status(200).json(products);
 }
