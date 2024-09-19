0f029424a44b12c9f85be299eefa1756243a4d16
Julia Hromada <hromada.julia@gmail.com>
2024-09-19 22:32:50 +0100

    read from database

--- backend-read/backend-read_products/components/ProductList/index.js
+++ backend-read/backend-read_products/components/ProductList/index.js
@@ -3,14 +3,14 @@ import { StyledHeading, StyledList } from "./ProductList.styled";
 import { StyledLink } from "../Link/Link.styled";
 
 export default function ProductList() {
-  const { data, isLoading } = useSWR("/api/products");
+  const { data, error } = useSWR("/api/products");
 
-  if (isLoading) {
-    return <h1>Loading...</h1>;
+  if (error) {
+    return <h2>Failed to load...</h2>;
   }
 
   if (!data) {
-    return;
+    return <h1>Loading...</h1>;
   }
 
   return (
@@ -18,8 +18,8 @@ export default function ProductList() {
       <StyledHeading>Available Fishes</StyledHeading>
       <StyledList>
         {data.map((product) => (
-          <li key={product.id}>
-            <StyledLink href={`/${product.id}`}>{product.name}</StyledLink>
+          <li key={product._id}>
+            <StyledLink href={`/${product._id}`}>{product.name}</StyledLink>
           </li>
         ))}
       </StyledList>
--- backend-read/backend-read_products/pages/api/products/index.js
+++ backend-read/backend-read_products/pages/api/products/index.js
@@ -1,5 +1,22 @@
-import { products } from "../../../lib/products";
+// import { products } from "../../../lib/products";
+import dbConnect from "@/db/connect";
+import Product from "@/db/models/Products";
 
-export default function handler(request, response) {
-  return response.status(200).json(products);
+export default async function handler(request, response) {
+  console.log("Connecting to database...");
+  await dbConnect();
+  console.log("Connected to database");
+
+  if (request.method === "GET") {
+    try {
+      const products = await Product.find();
+      console.log("Products found:", products);
+      return response.status(200).json(products);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      return response.status(500).json({ message: "Error fetching products" });
+    }
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
+  }
 }
